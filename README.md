d126d7dee4338e08966b6b23fbc4693a89d60ec3
asupanquo <quozul@outlook.com>
2022-06-05 13:47:57 +0000

    fix: running on ubuntu

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 ```sh
 sudo dnf install glfw-devel glew cmake
 ```
+
+### Ubuntu
+```sh
+sudo apt install cmake make gcc g++ git libopengl0 libglfw3 libglfw3-dev libglew-dev libglew2.1
+```
--- src/Quaternion.cpp
+++ src/Quaternion.cpp
@@ -2,7 +2,7 @@
 
 float *Quaternion::to_rotation_matrix() const {
     // Source: http://www.songho.ca/opengl/gl_quaternion.html
-    float *mat = new float[]{
+    float *mat = new float[16]{
             1 - 2 * (y * y) - 2 * (z * z),  2 * x * y - 2 * w * z,           2 * x * z + 2 * w * y,         0, // 1st column
             2 * x * y + 2 * w * z,          1 - 2 * (x * x) - 2 * (z * z),   2 * y * z - 2 * w * x,         0, // 2nd column
             2 * x * z - 2 * w * y,          2 * y * z + 2 * w * x,           1 - 2 * (x * x) - 2 * (y * y), 0,
